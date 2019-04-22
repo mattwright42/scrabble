a41bfa39498ddca632ff3849801939caa0b45292
Matthew Wright <matthewbwright@gmail.com>
2019-04-22 16:29:17 -0700

    nested loops are a function that can be called when a word is played

--- scrabble.py
+++ scrabble.py
@@ -28,12 +28,16 @@ player_to_words = {
 }
 
 player_to_points = {}
-for player, words in player_to_words.items():
-    player_points = 0
-    for word in words:
-        player_points += score_word(word)
-    player_to_points[player] = player_points
-print(player_to_points)
+
+
+def update_point_totals():
+    for player, words in player_to_words.items():
+        player_points = 0
+        for word in words:
+            player_points += score_word(word)
+        player_to_points[player] = player_points
+    update_point_totals()
+    print(player_to_points)
 
 
 def play_word(player, word):
